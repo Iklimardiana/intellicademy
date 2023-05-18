dc39cdfe0855e1450dde4d5495ffd17b9793f2b7
Fajar <fajars04@outlook.com>
2023-05-18 07:42:49 +0700

    install webpack + bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        clean: true
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ],
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './src'),
+        },
+        compress: true,
+        port: 3000,
+        liveReload: true
+    },  
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ]
+}
