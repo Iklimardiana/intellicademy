e6479cfa8b959b36ccac7f70c0cd401754139711
auryno19 <aurynoaga19@gmail.com>
2023-05-19 08:00:02 +0700

    add migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', [0, 1, 2])->default(2);
+            $table->string('phone');
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
