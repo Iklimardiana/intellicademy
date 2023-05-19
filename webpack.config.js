050f32a3a75bc45c86d933f6ee5d52cc010a6de2
Fajar <fajars04@outlook.com>
2023-05-19 14:48:33 +0700

    create student-dashboard-template

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,36 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-        clean: true
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './src'),
     },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ],
-            }
-        ]
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, './src'),
-        },
-        compress: true,
-        port: 3000,
-        liveReload: true
-    },  
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/template/index.html'
-        }),
-        new MiniCssExtractPlugin()
-    ]
-}
+    compress: true,
+    port: 3000,
+    liveReload: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template/student-dashboard.html',
+      //   filename: 'admin-dashboard.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
