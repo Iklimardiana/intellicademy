b952946b2117ac73b44613b6848dd95d46f8aed9
tzelginianc <tzelginia78@gmail.com>
2023-05-28 17:18:39 +0700

    view teacher

--- resources/views/teacher/course/view.blade.php
+++ resources/views/teacher/course/view.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('aside')
-@include('partials.aside')
+    @include('partials.aside')
 @endsection
 @section('content')
     <div aria-label="breadcrumb">
@@ -9,6 +9,37 @@
         </ol>
     </div>
 
-    <div class="card p-4">
+    {{-- <div class="card p-4">
     </div>
-    @endsection
+     --}}
+<div class="row">
+  <div class="col-sm-6 mb-3">
+    <div class="card">
+         <img src="/images/landingpage.png" class="card-img-top" alt="landingpage" style="max-height: 200px; object-fit:cover;">
+      <div class="card-body">
+        <h5 class="card-title">Nama Course</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est earum, sequi tempore nostrum non voluptatibus porro ipsum amet maxime consectetur.</p>
+        <div class="text-center">
+             <a href="#" class="btn btn-info">Students</a>
+        <a href="#" class="btn btn-primary">Modul</a>
+        <a href="#" class="btn btn-info">Assignment</a>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class="col-sm-6 mb-3">
+    <div class="card">
+         <img src="/images/landingpage.png" class="card-img-top" alt="landingpage" style="max-height: 200px; object-fit:cover;">
+      <div class="card-body">
+        <h5 class="card-title">Nama Course</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est earum, sequi tempore nostrum non voluptatibus porro ipsum amet maxime consectetur.</p>
+        <div class="text-center">
+             <a href="#" class="btn btn-info">Students</a>
+        <a href="#" class="btn btn-primary">Modul</a>
+        <a href="#" class="btn btn-info">Assignment</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
