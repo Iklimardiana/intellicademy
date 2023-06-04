62ba22991247f67cd5ff2f7f35a47d241f9f7e88
Iklima Mardiana <81839323+Iklimardiana@users.noreply.github.com>
2023-06-04 19:29:31 +0700

    remove intervention image

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -2,32 +2,14 @@
 
 namespace App\Helpers;
 
-use Intervention\Image\Facades\Image;
-
 class Helper
 {
     public static function processBase64Images($body)
     {
         $pattern = '/<img.*?src=["\'](data:image\/.*?;base64,.*?)["\'].*?>/i';
-        $processedBody = preg_replace_callback($pattern, function ($matches) {
-            $base64Image = $matches[1];
-            $compressedImage = self::compressBase64Image($base64Image);
-            $imgSrc = 'data:image/jpeg;base64,' . base64_encode($compressedImage);
-
-            return '<img src="' . $imgSrc . '" width="400">';
-        }, $body);
+        $replacement = '<img src="$1" width="400">';
+        $processedBody = preg_replace($pattern, $replacement, $body);
 
         return $processedBody;
-    }
-
-    private static function compressBase64Image($base64Image, $maxWidth = 700, $quality = 80)
-    {
-        $image = Image::make($base64Image);
-        $image->resize($maxWidth, null, function ($constraint) {
-            $constraint->aspectRatio();
-            $constraint->upsize();
-        });
-
-        return $image->encode('jpg', $quality);
     }
 }
