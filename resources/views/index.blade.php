2fe7b6ab0d333b4c2a1033963d74e5f29c2c8c4c
tzelginianc <tzelginia78@gmail.com>
2023-06-06 15:52:11 +0700

    fix index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.master')
 @section('aside')
-@include('partials.aside')
+     <aside class="col-lg-5 align-self-center">
+         <h1 class="text-center">
+             Lorem ipsum dolor sit amet.
+         </h1>
+         <p class="text-center">
+             Lorem ipsum dolor sit amet.
+         </p>
+         <div class="text-center">
+             <button class="btn btn-info">
+                 learn now!
+             </button>
+         </div>
+     </aside>
 @endsection
 @section('content')
 <div class="text-center">
