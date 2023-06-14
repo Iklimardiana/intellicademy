2062ed2de051a347ac84e002dede82aa1c166b0a
Iklima Mardiana <81839323+Iklimardiana@users.noreply.github.com>
2023-06-15 00:54:36 +0700

    fix card course view

--- resources/views/teacher/course/view.blade.php
+++ resources/views/teacher/course/view.blade.php
@@ -12,20 +12,22 @@
     <div class="row">
         @auth
             @forelse ($courses as $item)
-                <div class="col-sm-6 mb-3">
-                    <div class="card">
-                        <img src="{{ asset('images/thumbnail/'.$item->thumbnail) }}" class="card-img-top" alt="landingpage"
-                            style="max-height: 200px; object-fit: cover;">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $item->name }}</h5>
-                            <p class="card-text">{{ $item->description }}</p>
-                            <div class="text-center">
-                                <a href="/teacher/students/{{ $item->id }}" class="btn btn-info">Students</a>
-                                <a href="/teacher/modules/{{  $item->id }}" class="btn btn-primary">Modul</a>
-                            </div>
+            <div class="col-12 col-md-5 col-lg-6 pt-2">
+                <div class="card h-100">
+                    <div class="card-img-container" style="max-height: 200px; overflow: hidden;">
+                        <img src="{{ asset('images/thumbnail/'.$item->thumbnail) }}" class="card-img-top" alt="course image">
+                    </div>
+                    <div class="card-body d-flex flex-column justify-content-between">
+                        <h5 class="card-title">{{ $item->name }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ Str::rupiah($item->price) }}</h6>
+                        <p class="card-text flex-grow-1">{{ $item->description }}</p>
+                        <div class="text-center mt-auto">
+                            <a href="/teacher/students/{{ $item->id }}" class="btn btn-info">Students</a>
+                            <a href="/teacher/modules/{{ $item->id }}" class="btn btn-primary">Modul</a>
                         </div>
                     </div>
                 </div>
+            </div>
             @empty
                 <p>There Are No Courses For You</p>
             @endforelse
