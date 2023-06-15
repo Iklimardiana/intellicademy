64d52d4058a9e8149b232631c3c98a247bab8959
tzelginianc <tzelginia78@gmail.com>
2023-06-15 21:36:06 +0700

    fix view invoice

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -14,15 +14,6 @@
             margin-right: 20px;
         }
 
-        /* .navbar{
-            background-color: white;
-        } */
-
-        .rightNavbar {
-            left: auto !important;
-            right: 0px;
-        }
-
         .logo {
             height: 50px;
         }
@@ -32,80 +23,120 @@
             opacity: 75%;
         }
 
-        .rightInvoice {
-            float: right;
-        }
-        .ttd{
+        .ttd {
             height: 70px;
         }
     </style>
 </head>
 
 <body>
-    <nav class="navbar mt-3">
-        <div class="container-fluid">
-            <h2>Invoice</h2>
-            <div class="rightNavbar">
-                <h2>IntelliCademy <img class="logo ms-3" src="./images/logo.png" alt="logo"></h2>
-            </div>
-        </div>
-    </nav>
+
+    <div>
+        <table style="width: 100%;">
+            <colgroup>
+                <col span="1" style="width: 20%;">
+                <col span="1" style="width: 30%;">
+                <col span="1" style="width: 30%;">
+                <col span="1" style="width: 25%;">
+            </colgroup>
+            <tbody>
+                <tr>
+                    <td style="vertical-align: middle; ">
+                        <h2>IntelliCademy</h2>
+                    </td>
+                    <td style="vertical-align: middle; ">
+                        <h2>Invoice</h2>
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td style="vertical-align: middle; text-align: right">
+                        <img class="logo ms-3" src="./images/logo.png" alt="logo">
+                    </td>
+                </tr>
+            </tbody>
+
+        </table>
+
+
+    </div>
     <hr>
-    <nav class="navbar mt-3 mx-3">
-        <div class="container-fluid">
-            <div>
-                <h5><b>Kepada :</b></h5>
-                <p class="my-0">{{ $transaction->User->firstName }} {{ $transaction->User->lastName }}</p>
-                <p class="my-0">{{ $transaction->User->email }}</p>
-            </div>
-            <div class="rightNavbar">
-                <h5 class="mb-0">Tanggal : </h5>
-                <p class="my-0">{{ $date }}, {{ $month }} {{ $year }}</p>
-                <h5 class="mb-0 mt-2">Nomer Invoice</h5>
-                <p class="my-0">0{{ $transaction->id }}/0{{ $transaction->User->id }}/{{ $date }}{{ $monthNum }}{{ $year }}</p>
-            </div>
+    <div>
+        <div>
+            <h5><b>Kepada :</b></h5>
+            <p class="my-0">{{ $transaction->User->firstName }} {{ $transaction->User->lastName }}</p>
+            <p class="my-0">{{ $transaction->User->email }}</p>
         </div>
-    </nav>
-    <div class="justify-content-center d-flex mt-5">
-        <div class="w-50 text-center ">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Keterangan</th>
-                        <th scope="col">Deskripsi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">Course</th>
-                        <td>{{ $transaction->Course->name }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Jumla Modul</th>
-                        <td>{{ $transaction->Course->Module->count() }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Harga</th>
-                        <td>{{ Str::rupiah($transaction->Course->price) }}</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div>
+            <h5 class="mb-0 mt-2">Tanggal : </h5>
+            <p class="my-0">{{ $date }}, {{ $month }} {{ $year }}</p>
+        </div>
+        <div>
+            <h5 class="mb-0 mt-2">Nomer Invoice</h5>
+            <p class="my-0">
+                0{{ $transaction->id }}/0{{ $transaction->User->id }}/{{ $date }}{{ $monthNum }}{{ $year }}
+            </p>
         </div>
     </div>
 
-    <nav class="navbar mt-3 mx-3">
-        <div class="container-fluid">
-            <div>
-                <h5><b>Terimakasih Atas  <br>
-                    Pembayaran Course Anda</b></h5>
-            </div>
-            <div class="rightNavbar">
-                <h5 class="mb-0 mt-2">Hormat Kami</h5>
-                <img class="ttd"src="./images/ttd.jpeg" alt="ttd">
-                <p class="my-0">IntelliCademy</p>
-            </div>
-        </div>
-    </nav>
+    <div style="margin-top: 100px; margin-bottom: 100px">
+        <table class="desc" style=" margin-left: auto; margin-right: auto; width: 50%;  text-align: center">
+            <thead>
+                <tr>
+                    <th scope="col"
+                        style="height: 40px; background-color: rgba(176, 176, 176, 0.668); border: 1px solid rgba(176, 176, 176, 0.668);">
+                        Keterangan</th>
+                    <th scope="col"
+                        style="height: 40px; background-color: rgba(176, 176, 176, 0.668); border: 1px solid rgba(176, 176, 176, 0.668);">
+                        Deskripsi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row" style="height: 40px; border: 1px solid rgba(176, 176, 176, 0.668);">Course</th>
+                    <td style="height: 40px; border: 1px solid rgba(176, 176, 176, 0.668);">
+                        {{ $transaction->Course->name }}</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="height: 40px; border: 1px solid rgba(176, 176, 176, 0.668);">Jumlah Modul
+                    </th>
+                    <td style="height: 40px; border: 1px solid rgba(176, 176, 176, 0.668);">
+                        {{ $transaction->Course->Module->count() }}</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="height: 40px; border: 1px solid rgba(176, 176, 176, 0.668);">Harga</th>
+                    <td style="height: 40px; border: 1px solid rgba(176, 176, 176, 0.668);">
+                        {{ Str::rupiah($transaction->Course->price) }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div>
+        <table style="width: 100%;">
+            <tr>
+                <td></td>
+                <td style="text-align: right">
+                    <h5 class="mb-0 mt-2">Hormat Kami</h5>
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align: left vertical-align: middle; ">
+                    <h5><b>Terimakasih Atas <br>
+                            Pembayaran Course Anda</b></h5>
+                </td>
+                <td style="text-align: right">
+                    <img class="ttd"src="./images/ttd.jpeg" alt="ttd">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td style="text-align: right">
+                    <p class="my-0">IntelliCademy</p>
+                </td>
+            </tr>
+        </table>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
     </script>
