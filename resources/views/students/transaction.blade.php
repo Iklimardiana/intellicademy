2a96a024ea2e3adb669be543b8c0ad8e165ac7b9
Iklima Mardiana <81839323+Iklimardiana@users.noreply.github.com>
2023-06-15 18:39:15 +0700

    fix card view

--- resources/views/students/transaction.blade.php
+++ resources/views/students/transaction.blade.php
@@ -6,15 +6,17 @@
             @foreach ( $transaction as $t )
                 <div class="card col-12 col-md-5 col-lg-3 pt-2">
                     {{-- {{ dd($t->Course) }} --}}
-                    @if($t->Course->thumbnail == null)
-                        <img src="{{asset('/images/landingpage.png')}}" class="card-img-top" alt="course image">
-                    @else
-                        <img src="{{asset('/images/thumbnail/'. $t->Course->thumbnail)}}" class="card-img-top" alt="course image">
-                    @endif
-                    <div class="card-body">
+                    <div class="card-img-container">
+                        @if($t->Course->thumbnail == null)
+                            <img src="{{asset('/images/landingpage.png')}}" class="card-img-top" alt="course image">
+                        @else
+                            <img src="{{asset('/images/thumbnail/'. $t->Course->thumbnail)}}" class="card-img-top" alt="course image">
+                        @endif
+                    </div>
+                    <div class="card-body d-flex flex-column">
                         <h5 class="card-title">{{$t->Course->name}}</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">{{Str::rupiah($t->Course->price)}}</h6>
-                        <p class="card-text">{{$t->Course->description}}</p>
+                        <p class="card-text flex-grow-1">{{$t->Course->description}}</p>
                         <a href="/student/invoice/{{ $t->id }}" class="btn btn-success">Print Invoice</a>
                     </div>
                 </div>
